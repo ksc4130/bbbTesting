8dee3e296e8ba977df263a413823eabfdda309bd
Kyle Curren <ksc4130@gmail.com>
2013-10-20 15:32:15 -0400

    one

--- one.js
+++ one.js
@@ -1,22 +1,53 @@
 var fs = require('fs');
 
-var digPath = '/sys/class/gpio/gpio';
+var digPath = '/sys/class/gpio/gpio',
+    anPath = '/sys/devices/ocp.2/helper.14/',
+    ids = [
+        67,
+        'AIN4'
+    ];
 
-var pin = {
-    id: 67,
-    path: function () {
-        return digPath + this.id;
-    },
-    read: function () {
-        return fs.readFileSync(this.path() + '/value');
-    },
-    write: function (val) {
-        return fs.writeFileSync(this.path() + '/value', val);
-    }
-}
+var Pin = function (id) {
+    if(ids.indexOf(id) < 0)
+        return undefined;
 
-pin.write(1);
+    var self = this;
+
+    self.id = id;
+    if(typeof self.id === 'string')
+        self.path = digPath + self.id;
+    else
+        self.path = anPath + self.id;
+
+    return self;
+};
+
+
+Pin.prototype.read = function () {
+    return fs.readFileSync(this.path() + '/value');
+};
+
+Pin.prototype.write = function (val) {
+    return fs.writeFileSync(this.path() + '/value', val);
+};
+
+var pins = [
+    new Pin(67),
+    new Pin('AIN4')
+];
+
+pins[0].write(1);
 
 setTimeout(function () {
-    pin.write(0);
+    pins[0].write(0);
 }, 1000);
+
+
+fs.watch(pins[1].path + '/value', function (event, filename) {
+    console.log('event is: ' + event);
+    if (filename) {
+        console.log('filename provided: ' + filename);
+    } else {
+        console.log('filename not provided');
+    }
+});
