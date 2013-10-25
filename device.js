85329d170633475d92c70499b136cedd130443ca
Kyle Curren <ksc4130@gmail.com>
2013-10-25 15:37:30 -0400

    epoll test

--- device.js
+++ device.js
@@ -67,39 +67,44 @@
             return self;
         }
 
-        pinWork.exportPin(self.pin, self.direction, self.value, self.edge, args.ready);
+        self.init = function (d) {
+            self.ready(self);
+            if(self.actionType === 'switch') {
+                var Epoll = require('epoll').Epoll,
+                    fs = require('fs'),
+                    valuefd = fs.openSync( gpioPath + self.pin + '/value', 'r'),
+                    buffer = new Buffer(1);
 
-        if(self.actionType === 'switch') {
-            var Epoll = require('epoll').Epoll,
-                fs = require('fs'),
-                valuefd = fs.openSync( gpioPath + self.pin + '/value', 'r'),
-                buffer = new Buffer(1);
-
-            // Create a new Epoll. The callback is the interrupt handler.
-            var poller = new Epoll(function (err, fd, events) {
-                // Read GPIO value file. Reading also clears the interrupt.
-                fs.readSync(fd, buffer, 0, 1, 0);
-                console.log('poller', buffer[0]);
-                if(self.value[0] === one[0]) {
-                    console.log('poller a');
-                    if(buffer[0] === zero[0]) {
-                        console.log('poller b');
-                        //button was pressed do work
-                        emitter.emit('switched', self);
+                // Create a new Epoll. The callback is the interrupt handler.
+                var poller = new Epoll(function (err, fd, events) {
+                    // Read GPIO value file. Reading also clears the interrupt.
+                    fs.readSync(fd, buffer, 0, 1, 0);
+                    console.log('poller', buffer[0]);
+                    if(self.value[0] === one[0]) {
+                        console.log('poller a');
+                        if(buffer[0] === zero[0]) {
+                            console.log('poller b');
+                            //button was pressed do work
+                            emitter.emit('switched', self);
+                        }
                     }
-                }
 
-                self.value = buffer;
-                //console.log(events, fd, buffer.toString() === '1' ? 'pressed' : 'released');
-            });
+                    self.value = buffer;
+                    //console.log(events, fd, buffer.toString() === '1' ? 'pressed' : 'released');
+                });
 
-            // Read the GPIO value file before watching to
-            // prevent an initial unauthentic interrupt.
-            fs.readSync(valuefd, buffer, 0, 1, 0);
+                // Read the GPIO value file before watching to
+                // prevent an initial unauthentic interrupt.
+                fs.readSync(valuefd, buffer, 0, 1, 0);
 
-            // Start watching for interrupts.
-            poller.add(valuefd, Epoll.EPOLLPRI);
-        }
+                // Start watching for interrupts.
+                poller.add(valuefd, Epoll.EPOLLPRI);
+            }
+        };
+
+        pinWork.exportPin(self.pin, self.direction, self.value, self.edge, self.init);
+
+
 //        if(self.type === 'light') {
 //            if(self.actionType === 'onoff') {
 //
