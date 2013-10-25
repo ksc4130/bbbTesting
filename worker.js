8ef7e5b68667b9aa746decebd986818818d81c26
Kyle Curren <ksc4130@gmail.com>
2013-10-25 15:32:49 -0400

    epoll test

--- worker.js
+++ worker.js
@@ -1,4 +1,5 @@
-var device = require('./device'),
+var http = require('http'),
+    device = require('./device'),
     Device = device.Device;
 device.on('switched', function (d) {
    console.log('switched', d);
@@ -10,3 +11,7 @@ var d = new Device('68', {
         console.log('68 ready');
     }
 });
+
+var server = http.createServer(function (req, res) {
+
+}).listen(8080);
