0ff3e47d3099cd32e52071c694005a38822b5207
Kyle Curren <ksc4130@gmail.com>
2013-10-25 19:43:37 -0400

    epoll test

---
+++ house.js
@@ -0,0 +1,32 @@
+var worker = require('./worker'),
+    Device = require('./device').Device,
+    devices = [
+        new Device('67', {
+            name: 'Den Lights',
+            type: 'light',
+            actionType: 'onoff',
+            isVisible: true,
+            ready: function () {
+
+            }
+        }),
+        new Device('68', {
+            name: 'Garage Door',
+            type: 'overheadDoor',
+            actionType: 'momentary',
+            isVisible: true,
+            ready: function () {
+
+            }
+        }),
+        new Device('44', {
+            type: 'light',
+            actionType: 'switch',
+            controls: ['67'],
+            ready: function () {
+
+            }
+        })
+    ];
+
+worker.init(devices);
--- worker.js
+++ worker.js
@@ -23,68 +23,41 @@ device.on('onoff', function (d) {
         conn.emit('change', {id: d.id, state: d.value});
 });
 
-devices.push(
-    new Device('67', {
-        name: 'Den Lights',
-        type: 'light',
-        actionType: 'onoff',
-        isVisible: true,
-        ready: function () {
+module.exports.init = function (devs) {
+    devices = devs;
+    var io = require('socket.io-client');
+    var serverUrl = 'http://192.168.1.10:4131';
+    var conn = io.connect(serverUrl);
+    var secret = 'Askindl23@146Fscmaijnd523CXVWGN#63@#7efbsd23#$Rb';
 
-        }
-    }),
-    new Device('68', {
-        name: 'Garage Door',
-        type: 'overheadDoor',
-        actionType: 'momentary',
-        isVisible: true,
-        ready: function () {
+    conn.on('initWorker', function () {
+        conn.emit('initWorker', {devices: devices});
+    });
 
-        }
-    }),
-    new Device('44', {
-        type: 'light',
-        actionType: 'switch',
-        controls: ['67'],
-        ready: function () {
-
-        }
-    })
-);
-
-
-var io = require('socket.io-client');
-var serverUrl = 'http://192.168.1.10:4131';
-var conn = io.connect(serverUrl);
-var secret = 'Askindl23@146Fscmaijnd523CXVWGN#63@#7efbsd23#$Rb';
-
-conn.on('initWorker', function () {
-    conn.emit('initWorker', {devices: devices});
-});
-
-conn.on('devices', function (data) {
-    console.log('device for io server');
-    for(var i = 0, il = data.length; i < il; i++) {
-        for(var ic = 0, ilc = devices.length; ic < ilc; ic++) {
-            if(devices[ic].pin === data[i].pin) {
-                devices[ic].id = data[i].id;
+    conn.on('devices', function (data) {
+        console.log('device for io server');
+        for(var i = 0, il = data.length; i < il; i++) {
+            for(var ic = 0, ilc = devices.length; ic < ilc; ic++) {
+                if(devices[ic].pin === data[i].pin) {
+                    devices[ic].id = data[i].id;
+                }
             }
         }
-    }
-});
+    });
 
-conn.on('change', function (data) {
-    var device;
+    conn.on('change', function (data) {
+        var device;
 
-    for(var i = 0, il = devices.length; i < il; i++) {
-        if(devices[i].id.toString() === data.id.toString()) {
-            device = devices[i];
-            break;
+        for(var i = 0, il = devices.length; i < il; i++) {
+            if(devices[i].id.toString() === data.id.toString()) {
+                device = devices[i];
+                break;
+            }
         }
-    }
-    if(typeof device !== 'undefined' && device !== null) {
-        device.toggle(null);
-    } else
-        console.log("can't find device for id ", data.id);
+        if(typeof device !== 'undefined' && device !== null) {
+            device.toggle(null);
+        } else
+            console.log("can't find device for id ", data.id);
 
-});
+    });
+};
