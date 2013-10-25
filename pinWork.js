6b9e2b59a5403119ab5f816c281f7e9eee41ae80
Kyle Curren <ksc4130@gmail.com>
2013-10-25 15:49:45 -0400

    epoll test

--- pinWork.js
+++ pinWork.js
@@ -1,44 +1,31 @@
 (function () {
     var fs = require('fs'),
+        setupPin,
+        exportPin,
         exportPath = '/sys/class/gpio/export',
         gpioPath = '/sys/class/gpio/gpio';
 
-    var mod = {
-        setupPin: function (pin, direction, value, edge, fn) {
-            var workingPath = gpioPath + pin;
+    setupPin = function (pin, direction, value, edge, fn) {
+        var workingPath = gpioPath + pin;
 
-            if(typeof direction === 'string') {
-                fs.writeFile(workingPath +'/direction', direction, function (err) {
-                    if(err) {
-                        console.log('error setting direction for pin', pin);
-                        if(typeof fn === 'function') {
-                            fn(err);
-                        }
-                        return;
+        if(typeof direction === 'string') {
+            fs.writeFile(workingPath +'/direction', direction, function (err) {
+                if(err) {
+                    console.log('error setting direction for pin', pin);
+                    if(typeof fn === 'function') {
+                        fn(err);
                     }
-                    if(typeof value !== 'undefined' && value !== null) {
-                        fs.writeFile(workingPath +'/value', value, function (err) {
-                            if(err) {
-                                console.log('error setting value for pin', pin);
-                                if(typeof fn === 'function') {
-                                    fn(err);
-                                }
-                                return;
+                    return;
+                }
+                if(typeof value !== 'undefined' && value !== null) {
+                    fs.writeFile(workingPath +'/value', value, function (err) {
+                        if(err) {
+                            console.log('error setting value for pin', pin);
+                            if(typeof fn === 'function') {
+                                fn(err);
                             }
-                            if(typeof edge === 'string') {
-                                fs.writeFile(workingPath +'/edge', edge, function (err) {
-                                    if(err) {
-                                        console.log('error setting edge for pin', pin);
-                                        if(typeof fn === 'function') {
-                                            fn(err);
-                                        }
-                                        return;
-                                    }
-                                    fn(null);
-                                });
-                            }
-                        });
-                    } else {
+                            return;
+                        }
                         if(typeof edge === 'string') {
                             fs.writeFile(workingPath +'/edge', edge, function (err) {
                                 if(err) {
@@ -51,31 +38,47 @@
                                 fn(null);
                             });
                         }
-                    }
-                });
-            }
-        },
-        exportPin: function (pin, direction, value, edge, fn) {
-            var workingPath = gpioPath + pin;
-
-            fs.exists(workingPath, function (exists) {
-                if(!exists) {
-                    fs.writeFile(exportPath, pin, function (err) {
-                        if(err) {
-                            console.log('error exporting pin', pin);
-                            if(typeof fn === 'function') {
-                                fn(err);
-                            }
-                            return;
-                        }
-                        this.setupPin(pin, direction, value, edge, fn);
                     });
                 } else {
-                    this.setupPin(pin, direction, value, edge, fn);
+                    if(typeof edge === 'string') {
+                        fs.writeFile(workingPath +'/edge', edge, function (err) {
+                            if(err) {
+                                console.log('error setting edge for pin', pin);
+                                if(typeof fn === 'function') {
+                                    fn(err);
+                                }
+                                return;
+                            }
+                            fn(null);
+                        });
+                    }
                 }
             });
         }
-    }
+    };
 
-    module.exports = mod;
+    exportPin = function (pin, direction, value, edge, fn) {
+        var workingPath = gpioPath + pin;
+
+        fs.exists(workingPath, function (exists) {
+            if(!exists) {
+                fs.writeFile(exportPath, pin, function (err) {
+                    if(err) {
+                        console.log('error exporting pin', pin);
+                        if(typeof fn === 'function') {
+                            fn(err);
+                        }
+                        return;
+                    }
+                    setupPin(pin, direction, value, edge, fn);
+                });
+            } else {
+                this.setupPin(pin, direction, value, edge, fn);
+            }
+        });
+    };
+
+
+    exports.exportPin = exportPin;
+    exports.setupPin = setupPin;
 }());
