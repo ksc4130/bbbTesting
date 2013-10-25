b349efd4b5eef8556595adae00ce7d5b6ebdc569
Kyle Curren <ksc4130@gmail.com>
2013-10-25 19:48:51 -0400

    house

---
+++ barn.js
@@ -0,0 +1,24 @@
+var worker = require('./worker'),
+    Device = require('./device').Device,
+    devices = [
+        new Device('27', {
+            name: 'Barn Lights',
+            type: 'light',
+            actionType: 'onoff',
+            isVisible: true,
+            ready: function () {
+
+            }
+        }),
+        new Device('17', {
+            name: 'Barn Flood Lights',
+            type: 'light',
+            actionType: 'onoff',
+            isVisible: true,
+            ready: function () {
+
+            }
+        })
+    ];
+
+worker.init(devices);
