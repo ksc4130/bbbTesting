ba494e3fd2de9ee8c3df99357bb21e22ed5d9abc
Kyle Curren <ksc4130@gmail.com>
2013-10-25 20:42:17 -0400

    switched testing

--- device.js
+++ device.js
@@ -94,7 +94,7 @@
                 poller = new Epoll(function (err, fd, events) {
                     fs.readSync(fd, buffer, 0, 1, 0);
                     if(self.value[0] === one[0]) {
-                        console.log('switch aaaaaa*****', buffer[0]);
+                        console.log('switch aaaaaa*****', buffer[0], zero[0]);
                         if(buffer[0] === zero[0]) {
                             //button was pressed do work
                             emitter.emit('switched', self);
