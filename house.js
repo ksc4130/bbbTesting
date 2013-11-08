1bed2332fee52f5f7dede35586211da3472a0e25
Kyle Curren <ksc4130@gmail.com>
2013-11-08 13:17:10 -0500

    ana

--- house.js
+++ house.js
@@ -26,6 +26,24 @@ var worker = require('./worker'),
             ready: function () {
 
             }
+        }),
+        new Device('AIN3', {
+            name: 'Temp',
+            type: 'temp',
+            actionType: 'sensor',
+            isVisible: true,
+            ready: function () {
+
+            }
+        }),
+        new Device('AIN1', {
+            name: 'Light',
+            type: 'light',
+            actionType: 'sensor',
+            isVisible: true,
+            ready: function () {
+
+            }
         })
     ];
 
