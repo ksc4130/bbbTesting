a282d696f11c391a39ec229ba82d113535e4d4b8
Kyle Curren <ksc4130@gmail.com>
2013-11-20 22:40:40 -0500

    removed light and temp sensors

--- house.js
+++ house.js
@@ -28,7 +28,7 @@ var worker = require('./worker'),
         new Device('44', {
             type: 'light',
             actionType: 'switch',
-            controls: ['67'],
+            controls: ['67'],//den light
             ready: function () {
 
             }
@@ -71,25 +71,25 @@ var worker = require('./worker'),
             ready: function () {
 
             }
-        }),
-        new Device('AIN3', {
-            name: 'Temp',
-            type: 'temp',
-            actionType: 'sensor',
-            isVisible: true,
-            ready: function () {
-
-            }
-        }),
-        new Device('AIN1', {
-            name: 'Light',
-            type: 'light',
-            actionType: 'sensor',
-            isVisible: true,
-            ready: function () {
-
-            }
-        })
+        })//,
+//        new Device('AIN3', {
+//            name: 'Temp',
+//            type: 'temp',
+//            actionType: 'sensor',
+//            isVisible: true,
+//            ready: function () {
+//
+//            }
+//        }),
+//        new Device('AIN1', {
+//            name: 'Light',
+//            type: 'light',
+//            actionType: 'sensor',
+//            isVisible: true,
+//            ready: function () {
+//
+//            }
+//        })
     ];
 
 worker.init(devices);
