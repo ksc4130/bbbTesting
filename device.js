5c83503a6da541bc7a66df07b84fa634667274cd
Kyle Curren <ksc4130@gmail.com>
2013-12-20 01:04:47 -0500

    id fixes

--- device.js
+++ device.js
@@ -206,7 +206,7 @@
                         var lastTriggerDiff = Math.abs(self.lastTrigger - val);
                         var lastHighTriggerDiff = Math.abs(self.lastHighTrigger - val);
                         var lastLowTriggerDiff = Math.abs(self.lastHighTrigger - val);
-                        console.log('trigger diff', lastHighTriggerDiff, lastLowTriggerDiff, lastTriggerDiff);
+                        //console.log('trigger diff', lastHighTriggerDiff, lastLowTriggerDiff, lastTriggerDiff);
                         //set isHigh and isLow
                         if(self.forceTrigger || lastHighTriggerDiff >= self.highThershold) {
                             self.isHigh = self.value >= (self.trigger + self.highThershold);
